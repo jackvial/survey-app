16d411a8ee18dd6fb1f85a980c034fdf51c43e56
Jack Vial <vialjack@gmail.com>
2016-04-06 23:07:07 -0400

    Installed skelton css

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import SurveyList from './SurveyList';
+
+/**
+ * Top Level Component
+ * @author Jack Vial
+ */
 class App extends React.Component {
 	render(){
-		return (<div>Hi<SurveyList url="http://www.mocky.io/v2/56fcf6871000000e13aed252"></SurveyList></div>)
+		return (<div><SurveyList url="http://www.mocky.io/v2/56fcf6871000000e13aed252"></SurveyList></div>)
 	}
 }
 export default App
